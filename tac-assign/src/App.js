b32b73b50df17c612928c526a38aa2ece8003a0e
Sabasayed7786 <sabasayed7786@gmail.com>
2023-10-09 23:21:40 +0530

    first commit

---
+++ tac-assign/src/App.js
@@ -0,0 +1,15 @@
+import AdminUI from "./components/AdminUI";
+import "./App.css";
+
+
+const App = () => {
+ 
+
+  return (
+    <main>
+      <AdminUI />
+    </main>
+  );
+};
+
+export default App;
